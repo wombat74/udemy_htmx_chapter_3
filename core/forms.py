bdcef351966d31392c0354004a96d8453bb319d9
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2026-09-06 17:36:56 -0700

    added partials and set up first htmx call

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from core.models import Book
+
+class BookForm(forms.Form):
+    name = forms.CharField(
+        max_length=128,
+        widget=forms.TextInput(attrs={'class': 'input input-primary'})
+    )
+    genre = forms.ChoiceField(
+        choices=Book.GenreChoices.choices,
+        widget=forms.Select(attrs={'class': 'select select-primary'})
+    )
--- core/views.py
+++ core/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from core.forms import BookForm
+from core.models import Book
 
 @login_required
 def index(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid() == True:
+            name = form.cleaned_data['name']
+            genre = form.cleaned_data['genre']
+            book, _ = Book.objects.get_or_create(name=name, genre=genre)
+
+            if not request.user.books.filter(id=book.id).exists():
+                request.user.books.add(book)
+                return render(request, 'partials/book-row.html', {'book': book})
+
     books = request.user.books.all()
-    context = {'books': books}
+    context = {'books': books, 'form': BookForm()}
     return render(request, 'index.html', context)
