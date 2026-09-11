3e845ddab4b3bfd87f4d1a455e1e84f663d1ab13
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2026-09-11 15:08:32 -0700

    added sweetalert2 for pop-up

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete-book/<int:pk>/', views.delete_book, name='delete-book'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
 from core.forms import BookForm
 from core.models import Book
 
@@ -26,3 +28,12 @@ def index(request):
     books = request.user.books.all()
     context = {'books': books, 'form': BookForm()}
     return render(request, 'index.html', context)
+
+@login_required
+@require_http_methods(['DELETE'])
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    request.user.books.remove(book)
+    response = HttpResponse(status=204)
+    response['HX-Trigger'] = 'book-deleted'
+    return response
